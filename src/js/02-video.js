7e52ea83c6815d63535cfefcb978e91c9bce4082
MagdalenaBernat <magdalena.bernat@onet.pl>
2022-10-31 20:29:26 +0100

    added: vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,15 @@
 "use strict";
-const _ = require("lodash/throttle");
-const throttle = require('lodash.throttle');
+// const _ = require("lodash/throttle");
+// const throttle = require('lodash.throttle');
+const video = require("@vimeo/player");
+
+const 
+
+
+
+// const validator = require("validator");
+
+// const validateEmail = email => {
+//   return validator.isEmail(email);
+// };
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,4 @@
 "use strict";
-const _ = require("lodash");
-const throttle = require('lodash.throttle');
+
 
 
