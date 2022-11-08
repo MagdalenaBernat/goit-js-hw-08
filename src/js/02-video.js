58df12c40b16d5985d990a4d358d2254d86440d1
MagdalenaBernat <magdalena.bernat@onet.pl>
2022-11-08 09:18:56 +0100

    update video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,6 +5,9 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 player = new Player(iframe);
 
+const localStorageKey = "videoplayer-current-time";
+let currentTime = localStorage.getItem(localStorageKey);
+
 player.on('timeupdate', function (playedTime) {
     localStorage.setItem('playedTime', playedTime.seconds);
 });
